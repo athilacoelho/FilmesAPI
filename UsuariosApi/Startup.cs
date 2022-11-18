376e92ee2302da36d78b9a5ecdcd8702da959266
Athila da Silva Coelho <athila.coelho@cegas.com.br>
2022-11-18 17:48:53 -0300

    Email confirmation

--- UsuariosApi/Startup.cs
+++ UsuariosApi/Startup.cs
@@ -36,7 +36,10 @@ namespace UsuariosApi
             services.AddDbContext<UserDbContext>(options =>
                 options.UseMySQL(Configuration.GetConnectionString("UsuarioConnection"))
             );
-            services.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
+            services.AddIdentity<IdentityUser<int>, IdentityRole<int>>(
+                 
+                    opt => opt.SignIn.RequireConfirmedEmail = true
+                )
                 .AddEntityFrameworkStores<UserDbContext>();
             services.AddScoped<CadastroService, CadastroService>();
             services.AddScoped<LoginService, LoginService>();

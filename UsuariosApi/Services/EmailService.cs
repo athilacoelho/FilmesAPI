890789037a9aaadedbb205e7fa5353402b0a5aaf
Athila da Silva Coelho <athila.coelho@cegas.com.br>
2022-11-23 17:56:02 -0300

    final

--- UsuariosApi/Services/EmailService.cs
+++ UsuariosApi/Services/EmailService.cs
@@ -1,15 +1,92 @@
-﻿using System;
+﻿//using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UsuariosApi.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace UsuariosApi.Services
 {
     public class EmailService
     {
+        private IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void EnviarEmail(string[] destinatario, string assunto, int usuarioId, string code)
         {
             Mensagem mensagem = new Mensagem(destinatario, assunto, usuarioId, code);
+            var mensagemDeEmail = CriaCorpoDoEmail(mensagem);
+            Enviar(mensagemDeEmail);
         }
+
+        private void Enviar(MailMessage mensagemDeEmail)
+        {
+            var client = new SmtpClient(_configuration.GetValue<string>("EmailSettings:SmtpServer"), _configuration.GetValue<int>("EmailSettings:Port"))
+            {
+                Credentials = new NetworkCredential("fc12ad5327cf4c", "02a448251e3cfe"),
+                EnableSsl = true
+            };
+            try
+            {
+                client.Send(mensagemDeEmail);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private MailMessage CriaCorpoDoEmail(Mensagem mensagem)
+        {
+            var mensagemDeEmail = new MailMessage(_configuration.GetValue<string>("EmailSettings:From"),);
+            return mensagemDeEmail;
+        }
+
+        /*private MimeMessage CriaCorpoDoEmail(Mensagem mensagem)
+        {
+            var mensagemDeEmail = new MimeMessage();
+            mensagemDeEmail.From.Add(new MailboxAddress(_configuration.GetValue<string>("EmailSettings:From")));
+            mensagemDeEmail.To.AddRange(mensagem.Destinatario);
+            mensagemDeEmail.Subject = mensagem.Assunto;
+            mensagemDeEmail.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = mensagem.Conteudo
+            };
+            return mensagemDeEmail;
+        }
+
+        private void Enviar(MimeMessage mensagemDeEmail)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_configuration.GetValue<string>("EmailSettings:SmtpServer"),
+                        _configuration.GetValue<int>("EmailSettings:Port"), true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_configuration.GetValue<string>("EmailSettings:From"),
+                        _configuration.GetValue<string>("EmailSettings:Password"));
+                    client.Send(mensagemDeEmail);
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }*/
+
     }
 }
